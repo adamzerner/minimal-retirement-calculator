a94f38b4b21210b1be962a70eaf5536cce391ef2
Adam Zerner <adamzerner@protonmail.com>
2022-12-05 21:12:54 -0800

    Add slider for retirement buffer

--- components/index/inputs.tsx
+++ components/index/inputs.tsx
@@ -1,4 +1,5 @@
 import { Dispatch, SetStateAction } from "react";
+import { Flex } from "@chakra-ui/react";
 import { NumberInput } from "../number-input";
 
 type Props = {
@@ -32,54 +33,78 @@ export const Inputs = ({
 }: Props) => {
   return (
     <section>
-      <NumberInput
-        label="Monthly spending"
-        value={monthlySpending}
-        onChange={(_, v) => setMonthlySpending(v)}
-        min={0}
-        max={20000}
-        step={100}
-      />
-      <NumberInput
-        label="Additional yearly spending"
-        value={additionalYearlySpending}
-        onChange={(_, v) => setAdditionalYearlySpending(v)}
-        min={0}
-        max={1000000}
-        step={100}
-      />
-      <NumberInput
-        label="Current savings"
-        value={currentSavings}
-        onChange={(_, v) => setCurrentSavings(v)}
-        min={-1000000}
-        max={100000000}
-        step={1000}
-      />
-      <NumberInput
-        label="Savings per year"
-        value={savingsPerYear}
-        onChange={(_, v) => setSavingsPerYear(v)}
-        min={0}
-        max={1000000}
-        step={100}
-      />
-      <NumberInput
-        label="Withdrawal rate"
-        value={withdrawalRate}
-        onChange={(_, v) => setWithdrawalRate(v)}
-        min={0}
-        max={20}
-        step={0.25}
-      />
-      <NumberInput
-        label="Retirement buffer"
-        value={retirementBuffer}
-        onChange={(_, v) => setRetirementBuffer(v)}
-        min={0}
-        max={1000000}
-        step={100}
-      />
+      <Flex gap={5}>
+        <NumberInput
+          label="Monthly spending"
+          value={monthlySpending}
+          onChange={(_, v) => setMonthlySpending(v)}
+          min={0}
+          max={20000}
+          step={100}
+        />
+        <NumberInput
+          label="Additional yearly spending"
+          value={additionalYearlySpending}
+          onChange={(_, v) => setAdditionalYearlySpending(v)}
+          min={0}
+          max={1000000}
+          step={100}
+        />
+      </Flex>
+      <Flex gap={5}>
+        <NumberInput
+          label="Current savings"
+          value={currentSavings}
+          onChange={(_, v) => setCurrentSavings(v)}
+          min={-1000000}
+          max={100000000}
+          step={1000}
+        />
+        <NumberInput
+          label="Savings per year"
+          value={savingsPerYear}
+          onChange={(_, v) => setSavingsPerYear(v)}
+          min={0}
+          max={1000000}
+          step={100}
+        />
+      </Flex>
+      <Flex gap={5}>
+        <NumberInput
+          label={
+            <span>
+              Withdrawal Rate (
+              <a href="https://www.mrmoneymustache.com/2012/05/29/how-much-do-i-need-for-retirement/">
+                article
+              </a>
+              )
+            </span>
+          }
+          value={withdrawalRate}
+          onChange={(_, v) => setWithdrawalRate(v)}
+          min={0}
+          max={20}
+          step={0.25}
+          slider={{
+            min: 2,
+            max: 8,
+            step: 1,
+          }}
+        />
+        <NumberInput
+          label="Retirement buffer"
+          value={retirementBuffer}
+          onChange={(_, v) => setRetirementBuffer(v)}
+          min={0}
+          max={1000000}
+          step={1000}
+          slider={{
+            min: 0,
+            max: 200000,
+            step: 5000,
+          }}
+        />
+      </Flex>
     </section>
   );
 };
--- components/number-input.tsx
+++ components/number-input.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   NumberInput as NumberInputChakra,
   NumberInputField,
   NumberInputStepper,
@@ -6,15 +7,25 @@ import {
   NumberDecrementStepper,
   FormControl,
   FormLabel,
+  Slider,
+  SliderTrack,
+  SliderThumb,
+  SliderFilledTrack,
+  Flex,
 } from "@chakra-ui/react";
 
 type Props = {
-  label: string;
+  label: string | React.ReactNode;
   value: number;
   onChange: (valueAsString: string, valueAsNumber: number) => void;
   min?: number;
   max?: number;
   step?: number;
+  slider?: {
+    min: number;
+    max: number;
+    step: number;
+  };
 };
 
 export const NumberInput = ({
@@ -24,23 +35,98 @@ export const NumberInput = ({
   min = 0,
   max = 1000000,
   step = 1,
+  slider,
 }: Props) => {
   return (
     <FormControl mb="3">
       <FormLabel>{label}</FormLabel>
-      <NumberInputChakra
-        value={value}
-        onChange={onChange}
-        min={min}
-        max={max}
-        step={step}
-      >
-        <NumberInputField />
-        <NumberInputStepper>
-          <NumberIncrementStepper />
-          <NumberDecrementStepper />
-        </NumberInputStepper>
-      </NumberInputChakra>
+      {!!slider ? (
+        <NumberInputWithSlider
+          value={value}
+          onChange={onChange}
+          min={min}
+          max={max}
+          step={step}
+          slider={slider}
+        />
+      ) : (
+        <NumberInputWithoutSlider
+          value={value}
+          onChange={onChange}
+          min={min}
+          max={max}
+          step={step}
+        />
+      )}
     </FormControl>
   );
 };
+
+const NumberInputWithoutSlider = ({
+  value,
+  onChange,
+  min,
+  max,
+  step,
+}: Omit<Props, "label" | "slider">) => (
+  <NumberInputChakra
+    value={value}
+    onChange={onChange}
+    min={min}
+    max={max}
+    step={step}
+  >
+    <NumberInputField />
+    <NumberInputStepper>
+      <NumberIncrementStepper />
+      <NumberDecrementStepper />
+    </NumberInputStepper>
+  </NumberInputChakra>
+);
+
+const NumberInputWithSlider = ({
+  value,
+  onChange,
+  min,
+  max,
+  step,
+  slider,
+}: Required<Omit<Props, "label">>) => (
+  <Flex>
+    <NumberInputChakra
+      value={value}
+      onChange={onChange}
+      min={min}
+      max={max}
+      step={step}
+      maxW="125px"
+      mr="2rem"
+    >
+      <NumberInputField />
+      <NumberInputStepper>
+        <NumberIncrementStepper />
+        <NumberDecrementStepper />
+      </NumberInputStepper>
+    </NumberInputChakra>
+    <Slider
+      flex="1"
+      focusThumbOnChange={false}
+      value={value}
+      onChange={(v) => onChange(v.toString(), v)}
+      min={slider.min}
+      max={slider.max}
+      step={slider.step}
+    >
+      <SliderTrack>
+        <SliderFilledTrack />
+      </SliderTrack>
+      {slider.max > 99 ? (
+        <SliderThumb />
+      ) : (
+        <SliderThumb fontSize="sm" boxSize="32px">
+          {value}
+        </SliderThumb>
+      )}
+    </Slider>
+  </Flex>
+);
