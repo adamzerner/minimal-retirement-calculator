7fb3783f6d488eb6627bf5514e69e4aa40a2ed67
Adam Zerner <adamzerner@protonmail.com>
2022-12-05 21:39:27 -0800

    Add some preliminary content to the about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,20 @@
+import { Link } from "@chakra-ui/react";
+
 const About = () => {
-  return <main>About Page</main>;
+  return (
+    <main>
+      <p>
+        A little calculator to explore retirement considerations. Inspired by{" "}
+        <Link
+          isExternal
+          href="https://www.mrmoneymustache.com/2012/05/29/how-much-do-i-need-for-retirement/"
+        >
+          The 4% Rule: The Easy Answer to “How Much Do I Need for Retirement?”
+        </Link>
+        .
+      </p>
+    </main>
+  );
 };
 
 export default About;
