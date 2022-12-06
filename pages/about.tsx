75257cc687756bda7114750b5d4adc40d58a857f
Adam Zerner <adamzerner@protonmail.com>
2022-12-05 22:21:56 -0800

    Update about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,9 +1,11 @@
-import { Link } from "@chakra-ui/react";
+/* eslint-disable react/no-unescaped-entities */
+import { Link, Text, Heading } from "@chakra-ui/react";
 
 const About = () => {
   return (
     <main>
-      <p>
+      <Heading>About</Heading>
+      <Text mt={2}>
         A little calculator to explore retirement considerations. Inspired by{" "}
         <Link
           isExternal
@@ -12,7 +14,17 @@ const About = () => {
           The 4% Rule: The Easy Answer to “How Much Do I Need for Retirement?”
         </Link>
         .
-      </p>
+      </Text>
+      <Text mt={2}>
+        This calculator prioritizes minimalism over precision. If the "Years
+        until retirement" value is off by 12 months or so, for a lot of people
+        that's probably fine. They're just trying to get a solid ballpark
+        answer. If you do need precision, something like{" "}
+        <Link isExternal href="https://projectionlab.com/">
+          ProjectionLab
+        </Link>{" "}
+        would be a better choice.
+      </Text>
     </main>
   );
 };
