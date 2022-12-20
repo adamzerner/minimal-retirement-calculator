f7cafcd3ab5842c615fca65b46b64e03f326f723
Adam Zerner <adamzerner@protonmail.com>
2022-12-19 20:42:41 -0800

    Allow for underlining links

--- components/link.tsx
+++ components/link.tsx
@@ -1,8 +1,12 @@
 import { Link as ChakraLink } from "@chakra-ui/react";
 import NextLink from "next/link";
 
-export const Link = ({ children, ...props }: any) => (
-  <ChakraLink as={NextLink} {...props}>
+export const Link = ({ children, underline = true, ...props }: any) => (
+  <ChakraLink
+    as={NextLink}
+    sx={{ textDecoration: underline ? "underline" : "auto" }}
+    {...props}
+  >
     {children}
   </ChakraLink>
 );
